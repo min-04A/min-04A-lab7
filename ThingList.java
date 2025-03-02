da23660a0b0bc18a2701f9964fb81899b36a9ee3
Min <125743623+min-04A@users.noreply.github.com>
2025-03-02 18:54:26 -0500

    fourth commit

--- ThingList.java
+++ ThingList.java
@@ -1,4 +1,36 @@
-public class ThingList 
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThingList
 {
-    
+    private List<Thing> things;
+
+    public ThingList()
+    {
+        things = new LinkedList<>();
+    }
+
+    public void add(Thing t)
+    {
+        things.add(t);
+    }
+
+    public void moveAll()
+    {
+        for(Thing t : things)
+        {
+            t.move();
+        }
+    }
+
+    public void printAll()
+    {
+        for(Thing t : things)
+        {
+            System.out.println(t.row + " " + t.col + " " + t.lab);
+        }
+        System.out.println("done");
+        System.out.flush();
+    }
+
 }
